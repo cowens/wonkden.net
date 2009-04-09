868d6861247fdc3dfc5d9ae2db34f33d3906ea62
Chas. J. Owens IV <chas.owens@gmail.com>
2009-04-09 09:10:04 -0400

    added tar ball, fixed link, updated publish.sh

--- publish.sh
+++ publish.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 scp * wonkden.net:wonkden.net/html
+if [[ $? -ne 0 ]]; then
+	scp * wonkden.no-ip.org:wonkden.net
+	ssh wonkden.no-ip.org scp wonkden.net/* wonkden.net:wonkden.net/html
+fi
